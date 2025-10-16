6ff29c56e539042c7a7babb473eb65ec532be38d
Pratham <patadiyapratham98@gmail.com>
2025-10-16 14:58:31 +0530

    updated app.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -20,6 +20,7 @@ export const App = () => {
           <li>Jay</li>
           <li>Chetan</li>
           <li>Manthan</li>
+          <li>Pratham</li>
         </ul>
       </div>
       <div className="flex items-center justify-center mt-5">
